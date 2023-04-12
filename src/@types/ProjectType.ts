47f7bede1d4de8ea4e59b6905cb8487279876a54
APKAWY7PMABJWOMVRXDJ <admin@3440-mbp.gateway.sonic.net>
2023-04-11 21:44:40 -0700

    perf(devops): improve layout adaptibility, XXXXVIII

--- src/@types/ProjectType.ts
+++ src/@types/ProjectType.ts
@@ -2,7 +2,7 @@ export type ProjectType = {
   idUser: string
   title: string
   isActive: boolean
-  pathname: string
+  pathname?: string
   subtitle?: string
   description: string | string[]
   customer?: string
