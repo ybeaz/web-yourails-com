80bfec55e7eb311d1f79aef518d6fb12914bd16a
APKAWY7PMABJWOMVRXDJ <admin@3440-mbp.gateway.sonic.net>
2023-05-09 15:24:15 -0700

    perf(devops): own request of its own content II, VI

--- src/ContentMock/messagesMock.ts
+++ src/ContentMock/messagesMock.ts
@@ -2,8 +2,10 @@ import { MessageType } from '../@types/MessageType'
 
 import { messages01R } from './messages01RMock'
 import { messages02D } from './messages02DMock'
+import { messages03RD } from './messages03RDMock'
 
 export const messages: Omit<MessageType, 'position' | 'isTail'>[] = [
   ...messages01R,
   ...messages02D,
+  ...messages03RD,
 ]
