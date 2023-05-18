f2bdfad9f5d6c0c8e2adbb702696d0167e6d5648
APKAWY7PMABJWOMVRXDJ <admin@3440-mbp.gateway.sonic.net>
2023-05-17 21:22:22 -0700

    perf(devops): implement send message, V

--- src/DataLayer/index.saga.ts
+++ src/DataLayer/index.saga.ts
@@ -1,12 +1,14 @@
 import { all, fork } from 'redux-saga/effects'
 
 import templateSaga from './sagas/templateSaga'
+import addCompetencyTagsSaga from './sagas/addCompetencyTagsSaga'
 import addProjectsSaga from './sagas/addProjectsSaga'
 import addConversationSaga from './sagas/addConversationSaga'
 import initLoadingSaga from './sagas/initLoadingSaga'
 
 export default function* indexSaga() {
   yield all([fork(templateSaga)])
+  yield all([fork(addCompetencyTagsSaga)])
   yield all([fork(addProjectsSaga)])
   yield all([fork(addConversationSaga)])
   yield all([fork(initLoadingSaga)])
