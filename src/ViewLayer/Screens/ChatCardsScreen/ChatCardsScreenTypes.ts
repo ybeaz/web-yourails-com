119e2895353ef8171b0b26784c09eb2cbf7d17ef
Roman Ches <contact@yourails.com>
2023-09-18 11:24:26 -0700

    perf(develop): make it iOS compatible, X

---
+++ src/ViewLayer/Screens/ChatCardsScreen/ChatCardsScreenTypes.ts
@@ -0,0 +1,19 @@
+import { MediaParamsDefaultType } from '../../../YrlNativeViewLibrary'
+
+export interface ChatCardsScreenPropsType {
+  styleProps?: any
+  mediaParams?: MediaParamsDefaultType
+}
+
+export type ChatCardsScreenPropsOutType = Record<string, any>
+
+/**
+ * @import import { ChatCardsScreenType } from './ChatCardsScreenType'
+ */
+export interface ChatCardsScreenComponentType
+  extends React.FunctionComponent<ChatCardsScreenPropsType> {
+  (props: ChatCardsScreenPropsType): React.ReactElement
+}
+
+export type ChatCardsScreenType =
+  React.FunctionComponent<ChatCardsScreenPropsType>
---
+++ src/ViewLayer/Screens/ChatSpaceScreen/ChatSpaceScreenTypes.ts
@@ -0,0 +1,19 @@
+import { MediaParamsDefaultType } from '../../../YrlNativeViewLibrary'
+
+export interface ChatSpaceScreenPropsType {
+  styleProps?: any
+  mediaParams?: MediaParamsDefaultType
+}
+
+export type ChatSpaceScreenPropsOutType = Record<string, any>
+
+/**
+ * @import import { ChatSpaceScreenType } from './ChatSpaceScreenType'
+ */
+export interface ChatSpaceScreenComponentType
+  extends React.FunctionComponent<ChatSpaceScreenPropsType> {
+  (props: ChatSpaceScreenPropsType): React.ReactElement
+}
+
+export type ChatSpaceScreenType =
+  React.FunctionComponent<ChatSpaceScreenPropsType>
