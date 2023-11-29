e765e61315720a059ef5e46467e948571ba18ac2
Roman Ches <contact@yourails.com>
2023-11-29 12:42:31 -0800

    perf(develop): add minor changes, XXVI

--- src/CommunicationLayer/clients/axiosClient.ts
+++ src/CommunicationLayer/clients/axiosClient.ts
@@ -4,18 +4,14 @@ import { getDetectedEnv } from '../../Shared/getDetectedEnv'
 import { SERVERS_MAIN, ServersType } from '../../Constants/servers.const'
 import { getHeaders } from './getHeaders'
 
-const headers: Pick<
-  AxiosRequestHeaders,
-  'Access-Control-Allow-Origin' | 'Content-Type' | 'timestamp'
-> = getHeaders()
-
 const envType: string = getDetectedEnv()
 
 const baseURL = SERVERS_MAIN[envType as keyof ServersType] as string
 const { timeout } = SERVERS_MAIN
 
-export const axiosClient = axios.create({
-  baseURL: `${baseURL}`,
-  timeout,
-  headers,
-})
+export const axiosClient = () =>
+  axios.create({
+    baseURL: `${baseURL}`,
+    timeout,
+    headers: getHeaders({ printRes: false }),
+  })
